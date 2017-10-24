8c82b1703532d287844a477ef60db535a570b374
ArlenP <loloss>
2017-10-24 16:32:37 -0500

    Initial Commit

---
+++ Log In Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Log In Demo
+//
+//  Created by Arlen Peña Tolentino on 10/24/17.
+//  Copyright © 2017 Arlen Peña. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
