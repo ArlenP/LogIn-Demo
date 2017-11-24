c70692f87e503ec10bc6e9e2e8fd47370d2827d0
arlenp <arlenp@gmail.com>
2017-11-24 13:30:09 -0600

    Este es el primer Commit con la carga inicial del login

--- Log In Demo/ViewController.swift
+++ Log In Demo/ViewController.swift
@@ -7,13 +7,55 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet var textFiel: UITextField!
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var logInButton: UIButton!
+    @IBAction func logInButton(_ sender: Any) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let newValue = NSEntityDescription.insertNewObject(forEntityName: "Users",into: context)
+        newValue.setValue(textFiel.text, forKey: "name")
+        do {
+            try context.save()
+            textFiel.alpha = 0
+            logInButton.alpha = 0
+            label.alpha = 1
+            label.text = "Hola " + textFiel.text! + "!"
+        } catch  {
+            print("Fallo guardar")
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try context.fetch(request)
+                for result in results as! [NSManagedObject]{
+                    if let username = result.value(forKey: "name") as? String {
+                    textFiel.alpha = 0
+                    logInButton.alpha = 0
+                    label.alpha = 1
+                    label.text = "Hola " + username + "!"
+                }
+            }
+        } catch  {
+            print("Fallo respuesta")
+        }
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
